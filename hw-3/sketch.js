e4c515747c5b346b3219f8763df799a40e0af105
Manuela Cardenas de la Miyar <mao97mangaka@gmail.com>
2017-09-27 09:00:16 +0200

    Committing hw-3

---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+// I started writing the setup function
+function setup() {
+
+  // Then, I created a cavas (500px wide and 300px high)
+  createCanvas(500, 300);
+
+  //I declared the brackground color (203)
+  background(203);
+}
+
+
+// Declare the draw() function
+function draw(){
+
+  // I used the text function to write "Hello World!"
+  text("Hello World!", 200, 150);
+}
