ab02af26f47e5b02e6c4c6231f860d698484cdbb
Manuela Cardenas de la Miyar <mao97mangaka@gmail.com>
2017-11-03 06:49:07 +0100

    Committing hw-8

---
+++ hw-8/sketch.js
@@ -0,0 +1,16 @@
+var clicks = 3;
+
+function setup() {
+  createCanvas( windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(200);
+  var quanty = map(mouseY, 0, height, 20, 60);
+
+  if (option == 1) {
+    for (var x = 50; x)
+  }
+
+}
